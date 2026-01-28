e8991e5d5d5011caddd8ee364349fc3460ce8673
mengqyuan <mengqyuan@nuaa.edu.cn>
2026-01-28 12:45:47 +0800

    Update CaDRReS.py

--- code/CaDRReS.py
+++ code/CaDRReS.py
@@ -15,7 +15,7 @@ obj_function = widgets.Dropdown(options=['cadrres-wo-sample-bias', 'cadrres-wo-s
 model_spec_name = obj_function.value
 model_file = model_dir + '{}_param_dict.pickle'.format(model_spec_name)
 cadrres_model = model.load_model(model_file)
-gdsc_gene_exp_df = pd.read_csv('/data/home/yuanmengqin/CaDRReS-Sc/data/GDSC/GDSC_exp.tsv', sep='\t', index_col=0)
+gdsc_gene_exp_df = pd.read_csv('CaDRReS-Sc/data/GDSC/GDSC_exp.tsv', sep='\t', index_col=0)
 gdsc_gene_exp_df = gdsc_gene_exp_df.groupby(gdsc_gene_exp_df.index).mean()
 gdsc_norm_exp_df, gdsc_mean_exp_df = pp.gexp.normalize_log2_mean_fc(gdsc_gene_exp_df)
 ess_gene_list = utility.get_gene_list('CaDRReS-Sc/data/essential_genes.txt')
@@ -33,4 +33,5 @@ print('Predicting drug response using CaDRReS: {}'.format(model_spec_name))
 pred_df, P_test_df= model.predict_from_model(cadrres_model, test_kernel_df, model_spec_name)
 print('done!')
 print('Saving ' + model_dir + '{}_test_pred.csv'.format(model_spec_name))
+
 pred_df.to_csv("log2_IC50.pred.csv") #Predicted drug response (log2 IC50)
