c3ca043d0efe480bf3456d955cb1ed89b6e9fb49
mengqyuan <mengqyuan@nuaa.edu.cn>
2026-01-28 14:07:42 +0800

    Update FigS1.r

--- code/FigS1.r
+++ code/FigS1.r
@@ -1,7 +1,8 @@
-library(Seurat)
+library(Seurat) #
 load("SUM159.Rdata")
 SUM159 <- FindNeighbors(SUM159, reduction = "pca", dims = 1:10)
 SUM159=FindClusters(SUM159, resolution = 0.3)
 pdf("figs1.pdf")
 DimPlot(SUM159)
+
 dev.off()
